7cb819e5182fde1a2e631ebdf8d517bbface5a6e
joss <josseline.alvaradovargas@utxicotepec.edu.mx>
2023-07-08 00:22:11 -0600

    base actualizada

--- DBManager/BD_BOOK.sql
+++ DBManager/BD_BOOK.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `bookfash2023` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bookfash2023`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
--- Host: localhost    Database: bd_libros_bf
+-- Host: localhost    Database: bookfash2023
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,11 +25,11 @@ DROP TABLE IF EXISTS `autores`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `autores` (
-  `NombreAutor` varchar(255) NOT NULL,
-  `NombreLibro` varchar(255) NOT NULL,
-  `DescripciónAutor` varchar(255) DEFAULT NULL,
-  `ID_Autor` int unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ID_Autor`)
+  `id_autor` int NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `biografia_autor` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_autor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -41,6 +43,31 @@ LOCK TABLES `autores` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `genero`
+--
+
+DROP TABLE IF EXISTS `genero`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `genero` (
+  `id_genero` int NOT NULL,
+  `genero` varchar(100) DEFAULT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_genero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genero`
+--
+
+LOCK TABLES `genero` WRITE;
+/*!40000 ALTER TABLE `genero` DISABLE KEYS */;
+/*!40000 ALTER TABLE `genero` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `libros`
 --
 
@@ -48,11 +75,18 @@ DROP TABLE IF EXISTS `libros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `libros` (
-  `NombreLibro` varchar(255) NOT NULL,
-  `NombreAutor` varchar(255) NOT NULL,
-  `DescripciónLibro` varchar(255) DEFAULT NULL,
-  `ID_Libro` int unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ID_Libro`)
+  `id_libro` int NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `id_autor` int DEFAULT NULL,
+  `id_genero` int DEFAULT NULL,
+  `genero` varchar(100) DEFAULT NULL,
+  `año_publicacion` int DEFAULT NULL,
+  PRIMARY KEY (`id_libro`),
+  KEY `id_autor` (`id_autor`),
+  KEY `id_genero` (`id_genero`),
+  CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `autores` (`id_autor`),
+  CONSTRAINT `libros_ibfk_2` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -73,8 +107,16 @@ DROP TABLE IF EXISTS `recomendaciones`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recomendaciones` (
-  `NombreAutor` varchar(255) NOT NULL,
-  `NombreLibro` varchar(255) NOT NULL
+  `id_recomendacion` int NOT NULL,
+  `id_vista` int DEFAULT NULL,
+  `id_libro` int DEFAULT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `genero` varchar(100) DEFAULT NULL,
+  `descripcion_libro` varchar(0) DEFAULT NULL,
+  PRIMARY KEY (`id_recomendacion`),
+  KEY `id_libro` (`id_libro`),
+  CONSTRAINT `recomendaciones_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -95,10 +137,14 @@ DROP TABLE IF EXISTS `vistas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vistas` (
-  `ID_Libro` int unsigned NOT NULL AUTO_INCREMENT,
-  `NombreLibro` varchar(255) NOT NULL,
-  `NombreAutor` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID_Libro`)
+  `id_vista` int NOT NULL,
+  `id_libro` int DEFAULT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `fecha_vista` date DEFAULT NULL,
+  PRIMARY KEY (`id_vista`),
+  KEY `id_libro` (`id_libro`),
+  CONSTRAINT `vistas_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -120,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-11 18:49:27
+-- Dump completed on 2023-07-08  0:20:29

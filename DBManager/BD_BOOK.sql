4c768bd5fdc926a6d695815f9a84f43c3ea5b15e
JossAlvarado <84793967+JossAlvarado@users.noreply.github.com>
2023-07-10 13:05:16 -0600

    Update BD_BOOK.sql

--- DBManager/BD_BOOK.sql
+++ DBManager/BD_BOOK.sql
@@ -1,169 +1,104 @@
-CREATE DATABASE  IF NOT EXISTS `bookfash2023` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bookfash2023`;
--- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
---
--- Host: localhost    Database: bookfash2023
--- ------------------------------------------------------
--- Server version	8.0.33
+CREATE DATABASE bookfash;
+USE bookfash;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE Autor (
+    id_autor INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    totallibro INT,
+    biografiaAutor VARCHAR(100),
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo'
+);
 
---
--- Table structure for table `autores`
---
+CREATE TABLE Criterio (
+    id_criterio INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    descripcioncriterio VARCHAR(100),
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo'
+);
 
-DROP TABLE IF EXISTS `autores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `autores` (
-  `id_autor` int NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `biografia_autor` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_autor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE Editorial (
+    id_editorial INT PRIMARY KEY,
+    nombreeditorial VARCHAR(100),
+    autoresEditorial VARCHAR(100),
+    logoEditorial BLOB,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo'
+);
 
---
--- Dumping data for table `autores`
---
+CREATE TABLE Genero (
+    id_genero INT PRIMARY KEY,
+    nombreGenero VARCHAR(100),
+    subGenero VARCHAR(100),
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo'
+);
 
-LOCK TABLES `autores` WRITE;
-/*!40000 ALTER TABLE `autores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `autores` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE Libro (
+    id_libro INT PRIMARY KEY,
+    titulolibro VARCHAR(100),
+    portada BLOB,
+    id_autor INT,
+    id_criterio INT,
+    id_editorial INT,
+    id_genero INT,
+    numeroPaginas INT,
+    añoPublicacion INT,
+    DescripcionLibro VARCHAR(100),
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo',
+    FOREIGN KEY (id_autor) REFERENCES Autor(id_autor),
+    FOREIGN KEY (id_criterio) REFERENCES Criterio(id_criterio),
+    FOREIGN KEY (id_editorial) REFERENCES Editorial(id_editorial),
+    FOREIGN KEY (id_genero) REFERENCES Genero(id_genero)
+);
 
---
--- Table structure for table `genero`
---
+CREATE TABLE Persona (
+    id_persona INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    edad INT,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo'
+);
 
-DROP TABLE IF EXISTS `genero`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `genero` (
-  `id_genero` int NOT NULL,
-  `genero` varchar(100) DEFAULT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_genero`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE Recomendacion (
+    id_recomendacion INT PRIMARY KEY,
+    id_libro INT,
+    id_persona INT,
+    FOREIGN KEY (id_libro) REFERENCES Libro(id_libro),
+    FOREIGN KEY (id_persona) REFERENCES Persona(id_persona),
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo'
+);
 
---
--- Dumping data for table `genero`
---
+CREATE TABLE Usuario (
+    id_usuario INT PRIMARY KEY,
+    id_persona INT,
+    nombre VARCHAR(100),
+    correo VARCHAR(100),
+    contraseña VARCHAR(100),
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo'
+);
 
-LOCK TABLES `genero` WRITE;
-/*!40000 ALTER TABLE `genero` DISABLE KEYS */;
-/*!40000 ALTER TABLE `genero` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `libros`
---
-
-DROP TABLE IF EXISTS `libros`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `libros` (
-  `id_libro` int NOT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `id_autor` int DEFAULT NULL,
-  `id_genero` int DEFAULT NULL,
-  `genero` varchar(100) DEFAULT NULL,
-  `año_publicacion` int DEFAULT NULL,
-  PRIMARY KEY (`id_libro`),
-  KEY `id_autor` (`id_autor`),
-  KEY `id_genero` (`id_genero`),
-  CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `autores` (`id_autor`),
-  CONSTRAINT `libros_ibfk_2` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `libros`
---
-
-LOCK TABLES `libros` WRITE;
-/*!40000 ALTER TABLE `libros` DISABLE KEYS */;
-/*!40000 ALTER TABLE `libros` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `recomendaciones`
---
-
-DROP TABLE IF EXISTS `recomendaciones`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `recomendaciones` (
-  `id_recomendacion` int NOT NULL,
-  `id_vista` int DEFAULT NULL,
-  `id_libro` int DEFAULT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `genero` varchar(100) DEFAULT NULL,
-  `descripcion_libro` varchar(0) DEFAULT NULL,
-  PRIMARY KEY (`id_recomendacion`),
-  KEY `id_libro` (`id_libro`),
-  CONSTRAINT `recomendaciones_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `recomendaciones`
---
-
-LOCK TABLES `recomendaciones` WRITE;
-/*!40000 ALTER TABLE `recomendaciones` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recomendaciones` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `vistas`
---
-
-DROP TABLE IF EXISTS `vistas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `vistas` (
-  `id_vista` int NOT NULL,
-  `id_libro` int DEFAULT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `fecha_vista` date DEFAULT NULL,
-  PRIMARY KEY (`id_vista`),
-  KEY `id_libro` (`id_libro`),
-  CONSTRAINT `vistas_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id_libro`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `vistas`
---
-
-LOCK TABLES `vistas` WRITE;
-/*!40000 ALTER TABLE `vistas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vistas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-07-08  0:20:29
+CREATE TABLE Valoracion (
+    id_valoracion INT PRIMARY KEY,
+    id_libro INT,
+    id_persona INT,
+    numeroEstrellas INT,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estatus ENUM('Activo', 'Inactivo') DEFAULT 'Activo',
+    FOREIGN KEY (id_libro) REFERENCES Libro(id_libro),
+    FOREIGN KEY (id_persona) REFERENCES Persona(id_persona)
+);

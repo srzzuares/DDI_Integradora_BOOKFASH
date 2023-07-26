087315d75f60f5c2b00d2734631b6bf2d3ea556f
srzzuares@gmail.com <srzzuares@gmail.com>
2023-07-25 22:33:04 -0600

    route in tbb_criterio

--- Desarrollo_BackEnd/BackendAPI/routes/criterioRoute.py
+++ Desarrollo_BackEnd/BackendAPI/routes/criterioRoute.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from controllers.personaController import get_criterios,get_criterio,get_create,get_update,get_delete,get_deleteStatus
 criterioServer = APIRouter()
 
-#get persona
+#get criterio
 
 @server.get("/criterio/get_All") 
 def GET_ALL():
@@ -14,19 +14,19 @@ def GET_ONE(idcriterio):
     return get_criterio(idcriterio)
 
 @server.post("/criterio/insert")
-def POST_PERSON(data:criterioSchema):
+def POST_CRITERIO(data:criterioSchema):
     return get_create(data)
 
 @server.put("/criterio/update/{idcriterio}")
-def PUT_PERSON(data:criterioSchemaPut,idcriterio):
+def PUT_CRITERIO(data:criterioSchemaPut,idcriterio):
     return get_update(data,idcriterio)
 
 @server.delete("/criterio/deleteSt/{idcriterio}")
-def DEL_PERSONSTATUS(idcriterio):
+def DEL_CRITERIOSTATUS(idcriterio):
     return get_deleteStatus(idcriterio)
 
 @server.delete("/criterio/delete/{idcriterio}")
-def DEL_PERSON(idcriterio): 
+def DEL_CRITERIO(idcriterio): 
     return get_delete(idcriterio)
 
 

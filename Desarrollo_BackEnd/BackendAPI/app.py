fa71f0a52614a811dca568a93e07af0065066f92
srzzuares@gmail.com <srzzuares@gmail.com>
2023-07-25 22:46:53 -0600

    import of route criterio server in app.py

--- Desarrollo_BackEnd/BackendAPI/app.py
+++ Desarrollo_BackEnd/BackendAPI/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.personaRoute import server
 from routes.generoRoute import gen
+from routes.criterioRoute import criterioServer
 app = FastAPI()
 
 
 
 app.include_router(server)
 app.include_router(gen)
+app.include_router(criterioServer)

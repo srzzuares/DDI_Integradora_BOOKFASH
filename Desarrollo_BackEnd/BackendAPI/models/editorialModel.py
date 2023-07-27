ba35a0472db36283e3d9c1d606ac58ab3d5160f8
srzzuares@gmail.com <srzzuares@gmail.com>
2023-07-26 20:53:51 -0600

    Code Model of tbb_editorial

---
+++ Desarrollo_BackEnd/BackendAPI/models/editorialModel.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Table, Column, Integer, String, Date, DateTime,Enum
+from config.db import meta, engine
+import datetime
+editorial = Table("tbb_editorial", meta,
+         Column("id_editorial", Integer, primary_key=True, autoincrement=True, nullable=False),
+         Column("nombreeditorial", String(100), nullable=False),
+         Column("autoresEditorial", String(100), default='Autor Anonimo'),
+         Column("logoEditorial", String(250), nullable=True), # longblob
+         Column("estatus", Enum('Activo', 'Inactivo'), nullable=False, default='Activo'),
+         Column("fecha_actualizacion", DateTime, nullable=True),
+         Column("fecha_registro", default=datetime.datetime.utcnow)
+         )
+
+meta.create_all(engine)
